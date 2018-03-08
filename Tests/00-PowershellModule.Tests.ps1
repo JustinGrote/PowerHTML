8e3fff0df8440d2bfff836eca6247a1000ed5d56
JustinGrote <justingrote@github.com>
2018-03-07 16:15:15 -0800

    Update to latest tests

--- Tests/00-PowershellModule.Tests.ps1
+++ Tests/00-PowershellModule.Tests.ps1
@@ -1,13 +1,18 @@
 #requires -module BuildHelpers
+if (-not import-module BuildHelpers -PassThru -erroraction silentlycontinue) {
+    install-module buildhelpers -scope currentuser -erroraction stop -force
+    import-module BuildHelpers -erroraction stop
+}
+
 $PSVersion = $PSVersionTable.PSVersion.Major
 $BuildOutputProject = Join-Path $env:BHBuildOutput $env:BHProjectName
-Describe "Module Build" {
-    $ModuleManifestPath = Join-Path $BuildOutputProject "\*.psd1"
 
+Describe 'Powershell Module' {
+    $ModuleManifestPath = Join-Path $BuildOutputProject '\*.psd1'
     Context "$env:BHProjectName" {
         $ModuleName = $env:BHProjectName
-        It "Has a valid Module Manifest" {
-            if ($isCoreCLR) {
+        It 'Has a valid Module Manifest' {
+            if ($isCoreCLR -or $PSVersionTable.PSVersion -ge [Version]"5.1") {
                 $Script:Manifest = Test-ModuleManifest $ModuleManifestPath
             } else {
                 #Copy the Module Manifest to a temp file in order to test to fix a bug where
@@ -19,25 +24,25 @@ Describe "Module Build" {
             }
         }
 
-        It "Has a valid root module" {
+        It 'Has a valid root module' {
             $Manifest.RootModule | Should Be "$ModuleName.psm1"
         }
 
-        It "Has a valid Description" {
+        It 'Has a valid Description' {
             $Manifest.Description | Should Not BeNullOrEmpty
         }
 
-        It "Has a valid GUID" {
+        It 'Has a valid GUID' {
             [Guid]$Manifest.Guid | Should BeOfType 'System.GUID'
         }
 
-        It "Has a valid Copyright" {
+        It 'Has a valid Copyright' {
             $Manifest.Copyright | Should Not BeNullOrEmpty
         }
 
         It 'Exports all public functions' {
-            $FunctionFiles = Get-ChildItem "$BuildOutputProject\Public" -Filter *.ps1 | Select-Object -ExpandProperty BaseName
-            $FunctionNames = $FunctionFiles | ForEach-Object {$_ -replace '-', "-$($Manifest.Prefix)"}
+            $FunctionFiles = Get-ChildItem "$BuildOutputProject\Public" -Filter *.ps1 | Select -ExpandProperty BaseName
+            $FunctionNames = $FunctionFiles | foreach {$_ -replace '-', "-$($Manifest.Prefix)"}
             $ExFunctions = $Manifest.ExportedFunctions.Values.Name
             foreach ($FunctionName in $FunctionNames)
             {
@@ -45,21 +50,24 @@ Describe "Module Build" {
             }
         }
 
-        It "Has at least 1 exported command" {
+        It 'Has at least 1 exported command' {
             $Script:Manifest.exportedcommands.count | Should BeGreaterThan 0
         }
-        It "Can be imported as a module successfully" {
+        It 'Can be imported as a module successfully' {
+            Remove-Module $ModuleName -ErrorAction SilentlyContinue
             Import-Module $BuildOutputProject -PassThru -OutVariable BuildOutputModule | Should BeOfType System.Management.Automation.PSModuleInfo
             $BuildOutputModule.Name | Should Be $ModuleName
         }
-        It "Is visible in Get-Module" {
-            (Get-Module $ModuleName).Name | Should Be $ModuleName
+        It 'Is visible in Get-Module' {
+            $module = Get-Module $ModuleName
+            $Module | Should BeOfType System.Management.Automation.PSModuleInfo
+            $Module.Name | Should Be $ModuleName
         }
     }
 }
 
-Describe "PSScriptAnalyzer" {
-    $results = Invoke-ScriptAnalyzer -Path $BuildOutputProject -Recurse -ExcludeRule "PSAvoidUsingCmdletAliases"
+Describe 'PSScriptAnalyzer' {
+    $results = Invoke-ScriptAnalyzer -Path $BuildOutputProject -Recurse -ExcludeRule "PSAvoidUsingCmdletAliases" -Verbose:$false
     It 'PSScriptAnalyzer returns zero errors for all files in the repository' {
         $results.Count | Should Be 0
     }
